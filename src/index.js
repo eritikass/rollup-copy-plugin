002e6a342a3b4f5c5673dea52debd25651221602
Siim Tiilen <eritikass@gmail.com>
2018-07-14 18:38:36 +0300

    update project echo-system

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ module.exports = function rollupCopyPlugin(options) {
   return {
     ongenerate() {
       Object.keys(options).forEach(function copy(src) {
+        // eslint-disable-next-line no-console
         console.log(`copy: ${options[src]} > ${src}`);
         fs.writeFileSync(options[src], fs.readFileSync(src));
       });
